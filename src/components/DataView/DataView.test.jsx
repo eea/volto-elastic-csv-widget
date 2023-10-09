8ba49b38b74a012bb2a54c08a4994d5b90517dc4
Andrei Grigore <andrei.ggr@gmail.com>
2023-10-09 10:45:24 +0300

    chore: more test cases 4 code-coverage

--- src/components/DataView/DataView.test.jsx
+++ src/components/DataView/DataView.test.jsx
@@ -1,57 +1,75 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
 import DataView from './DataView';
 
-window.URL.createObjectURL = jest.fn(() => 'test');
-
 describe('DataView', () => {
-  const mockDataTable = {
-    content_type: 'Shipping and Ports Measure',
-    fields: [],
-    hits: [],
-    index: 'wisetest_searchui',
-    website: 'wise-marine',
-  };
-
   it('should render the component', () => {
-    const { container } = render(<DataView tableData={mockDataTable} />);
-    expect(
-      container.querySelector('.elastic-connector-view'),
-    ).toBeInTheDocument();
+    const mockData = {
+      column1: ['data1', 'data2'],
+    };
+    render(<DataView tableData={mockData} />);
+    expect(screen.getByText('data1')).toBeInTheDocument();
   });
 
   it('should display "No compatible data" message when there is no data', () => {
-    const { getByText } = render(<DataView tableData={{}} />);
-    expect(getByText('No compatible data')).toBeInTheDocument();
+    render(<DataView tableData={{}} />);
+    expect(screen.getByText('No compatible data')).toBeInTheDocument();
   });
 
-  it('should render table headers correctly', () => {
-    const mockDataWithHeaders = {
+  it('should handle undefined columns gracefully', () => {
+    const mockDataWithUndefined = {
       column1: ['data1', 'data2'],
-      column2: ['data3', 'data4'],
+      undefined: ['data3', 'data4'],
     };
-    const { getAllByRole } = render(
-      <DataView tableData={mockDataWithHeaders} />,
-    );
-    const headers = getAllByRole('columnheader');
+    render(<DataView tableData={mockDataWithUndefined} />);
+    const headers = screen.getAllByRole('columnheader');
+    expect(headers).toHaveLength(1); // Only 1 valid header
+    expect(screen.getByText('data1')).toBeInTheDocument();
+    expect(screen.getByText('data2')).toBeInTheDocument();
+    expect(screen.queryByText('data3')).not.toBeInTheDocument();
+    expect(screen.queryByText('data4')).not.toBeInTheDocument();
+  });
+
+  it('should handle columns with no data gracefully', () => {
+    const mockDataWithEmptyColumn = {
+      column1: ['data1', 'data2'],
+      column2: [],
+    };
+    render(<DataView tableData={mockDataWithEmptyColumn} />);
+    const headers = screen.getAllByRole('columnheader');
     expect(headers).toHaveLength(2);
-    expect(headers[0]).toHaveTextContent('column1');
-    expect(headers[1]).toHaveTextContent('column2');
+    expect(screen.getByText('data1')).toBeInTheDocument();
+    expect(screen.getByText('data2')).toBeInTheDocument();
   });
 
-  it('should render table rows correctly', () => {
-    const mockDataWithRows = {
+  it('should render all rows and columns correctly', () => {
+    const mockData = {
       column1: ['data1', 'data2'],
       column2: ['data3', 'data4'],
     };
-    const { getAllByRole } = render(<DataView tableData={mockDataWithRows} />);
-    const rows = getAllByRole('row');
+    render(<DataView tableData={mockData} />);
+    const headers = screen.getAllByRole('columnheader');
+    const cells = screen.getAllByRole('cell');
+    expect(headers).toHaveLength(2);
+    expect(cells).toHaveLength(4);
+    expect(screen.getByText('data1')).toBeInTheDocument();
+    expect(screen.getByText('data2')).toBeInTheDocument();
+    expect(screen.getByText('data3')).toBeInTheDocument();
+    expect(screen.getByText('data4')).toBeInTheDocument();
+  });
+
+  it('should handle rows with varied lengths', () => {
+    const mockDataWithDifferentLengths = {
+      column1: ['data1', 'data2'],
+      column2: ['data3'],
+    };
+    render(<DataView tableData={mockDataWithDifferentLengths} />);
+    const rows = screen.getAllByRole('row');
     expect(rows).toHaveLength(3); // 2 data rows + 1 header row
-    expect(rows[1]).toHaveTextContent('data1');
-    expect(rows[1]).toHaveTextContent('data3');
-    expect(rows[2]).toHaveTextContent('data2');
-    expect(rows[2]).toHaveTextContent('data4');
+    expect(screen.getByText('data1')).toBeInTheDocument();
+    expect(screen.getByText('data2')).toBeInTheDocument();
+    expect(screen.getByText('data3')).toBeInTheDocument();
+    expect(screen.queryByText('data4')).not.toBeInTheDocument();
   });
 });
--- src/helpers.test.jsx
+++ src/helpers.test.jsx
@@ -22,6 +22,43 @@ describe('Helper Functions', () => {
         'testField.keyword',
       );
     });
+
+    it('should modify mustQueries when objectProvides is specified', () => {
+      const payload = createAggregatedPayload({
+        objectProvides: 'providesObject',
+      });
+      expect(payload.query.bool.must).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            bool: expect.objectContaining({
+              should: expect.arrayContaining([
+                expect.objectContaining({
+                  term: { objectProvides: 'providesObject' },
+                }),
+              ]),
+              minimum_should_match: 1,
+            }),
+          }),
+        ]),
+      );
+    });
+
+    it('should include second level aggregations when specified', () => {
+      const payload = createAggregatedPayload({
+        use_aggs: true,
+        agg_fields: [
+          {
+            field: 'testField',
+            secondLevelAgg: 'secondLevelTestField',
+          },
+        ],
+      });
+
+      expect(payload.aggs).toHaveProperty(
+        'testField.aggs.secondLevelTestField.terms.field',
+        'secondLevelTestField.keyword',
+      );
+    });
   });
 
   describe('buildTableFromFields', () => {
@@ -38,6 +75,19 @@ describe('Helper Functions', () => {
       expect(table).toEqual({
         field1: ['value1', 'value3'],
         'Field Two': ['value2', 'value4'],
+      });
+    });
+
+    it('should handle items missing a field', () => {
+      const items = [
+        { _source: { field1: 'value1', field2: 'value2' } },
+        { _source: { field1: 'value3' } },
+      ];
+      const fields = [{ field: 'field1' }, { field: 'field2' }];
+      const table = buildTableFromFields(items, fields);
+      expect(table).toEqual({
+        field1: ['value1', 'value3'],
+        field2: ['value2', undefined],
       });
     });
   });
@@ -58,6 +108,40 @@ describe('Helper Functions', () => {
         'Test Field values': ['key1', 'key2'],
         'Test Field total': [10, 20],
       });
+    });
+
+    it('should handle second-level aggregations', () => {
+      const data = [
+        {
+          testField: [
+            {
+              key: 'key1',
+              doc_count: 10,
+              secondLevelTestField: {
+                buckets: [
+                  { key: 'subKey1', doc_count: 5 },
+                  { key: 'subKey2', doc_count: 5 },
+                ],
+              },
+            },
+          ],
+        },
+      ];
+      const fields = [
+        { field: 'testField', secondLevelAgg: 'secondLevelTestField' },
+      ];
+      const table = buildTableFromAggs(data, fields);
+      expect(table).toEqual({
+        testField_values: ['key1'],
+        testField_total: [10],
+        subKey1: [5],
+        subKey2: [5],
+      });
+    });
+
+    it('should return an empty object when non-array data is provided', () => {
+      const table = buildTableFromAggs({}, []);
+      expect(table).toEqual({});
     });
   });
 });
