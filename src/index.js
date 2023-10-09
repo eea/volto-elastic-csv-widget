e44c024f16b50bbe44ba20baa172b62fcfbd13e4
Andrei Grigore <andrei.ggr@gmail.com>
2023-10-09 15:41:08 +0300

    fix: lint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import ConnectedElasticConnectorView from './components/Views/ElasticConnectorView/ConnectedElasticConnectorView';
+import EsConnectorView from './components/Views/ElasticConnectorView/ConnectedElasticConnectorView';
 import Elastic2CSVWidget from './components/Widgets/Elastic2CSVWidget';
 
 const applyConfig = (config) => {
   //elasticconnector view
-  config.views.contentTypesViews.elasticconnector =
-    ConnectedElasticConnectorView;
+  config.views.contentTypesViews.elasticconnector = EsConnectorView;
 
   //elastic connector elastic_csv_widget
   config.widgets.id.elastic_csv_widget = Elastic2CSVWidget;
