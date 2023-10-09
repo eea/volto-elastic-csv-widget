aaac1b6460da7d711236adb7dfb2f3fc1d808fb4
Andrei Grigore <andrei.ggr@gmail.com>
2023-10-09 11:58:44 +0300

    chore: more tests to please code-coverage

--- src/components/DataView/DataView.test.jsx
+++ src/components/DataView/DataView.test.jsx
@@ -60,4 +60,52 @@ describe('DataView', () => {
     expect(screen.getByText('data3')).toBeInTheDocument();
     expect(screen.queryByText('data4')).not.toBeInTheDocument();
   });
+
+  // New test cases to enhance coverage
+  it('should render the column headers correctly', () => {
+    const mockData = {
+      'Origin values': ['BD', 'HD'],
+      'Origin total': [2260, 1865],
+    };
+    render(<DataView tableData={mockData} />);
+    expect(screen.getByText('Origin values')).toBeInTheDocument();
+    expect(screen.getByText('Origin total')).toBeInTheDocument();
+  });
+
+  it('should render data correctly when tableData prop is null', () => {
+    render(<DataView tableData={null} />);
+    expect(screen.getByText('No compatible data')).toBeInTheDocument();
+  });
+
+  it('should render data correctly when tableData prop is not provided', () => {
+    render(<DataView />);
+    expect(screen.getByText('No compatible data')).toBeInTheDocument();
+  });
+
+  it('should render each cell correctly', () => {
+    const mockData = {
+      'Origin values': ['BD', 'HD'],
+    };
+    render(<DataView tableData={mockData} />);
+    expect(screen.getByText('BD')).toBeInTheDocument();
+    expect(screen.getByText('HD')).toBeInTheDocument();
+  });
+
+  it('should handle empty strings in data gracefully', () => {
+    const mockData = {
+      'Origin values': ['BD', ''],
+    };
+    render(<DataView tableData={mockData} />);
+    expect(screen.getByText('BD')).toBeInTheDocument();
+    // Ensure an empty cell doesn't throw an error
+  });
+
+  it('should handle null values in data gracefully', () => {
+    const mockData = {
+      'Origin values': ['BD', null],
+    };
+    render(<DataView tableData={mockData} />);
+    expect(screen.getByText('BD')).toBeInTheDocument();
+    // Ensure a null cell doesn't throw an error
+  });
 });
--- src/helpers.test.jsx
+++ src/helpers.test.jsx
@@ -12,6 +12,49 @@ describe('Helper Functions', () => {
       expect(payload).toHaveProperty('track_total_hits', true);
     });
 
+    it('should handle all provided parameters', () => {
+      const payloadConfig = {
+        objectProvides: 'someObject',
+        cluster_name: 'someCluster',
+        index: 'someIndex',
+        size: 200,
+        use_aggs: true,
+        agg_fields: [
+          { field: 'someField', secondLevelAgg: 'someSecondLevelAgg' },
+        ],
+      };
+      const payload = createAggregatedPayload(payloadConfig);
+      expect(payload).toHaveProperty('index', 'someIndex');
+      expect(payload).toHaveProperty('size', 200);
+      expect(payload.query.bool.must).toHaveLength(6); // Adjust as per expected length
+    });
+
+    it('should use custom size if provided', () => {
+      const payload = createAggregatedPayload({ size: 50 });
+      expect(payload).toHaveProperty('size', 50);
+    });
+
+    it('should create appropriate mustQueries with objectProvides and cluster_name', () => {
+      const payload = createAggregatedPayload({
+        objectProvides: 'someObject',
+        cluster_name: 'someCluster',
+      });
+      expect(payload.query.bool.must).toContainEqual(
+        expect.objectContaining({
+          bool: expect.objectContaining({
+            should: [{ term: { objectProvides: 'someObject' } }],
+          }),
+        }),
+      );
+      expect(payload.query.bool.must).toContainEqual(
+        expect.objectContaining({
+          bool: expect.objectContaining({
+            should: [{ term: { cluster_name: 'someCluster' } }],
+          }),
+        }),
+      );
+    });
+
     it('should add aggregations based on agg_fields', () => {
       const payload = createAggregatedPayload({
         use_aggs: true,
@@ -73,55 +116,87 @@ describe('Helper Functions', () => {
   });
 
   describe('buildTableFromAggs', () => {
-    it('should build a table from provided data and fields', () => {
-      const data = [
-        {
-          testField: [
-            { key: 'key1', doc_count: 10 },
-            { key: 'key2', doc_count: 20 },
-          ],
-        },
-      ];
+    it('should handle empty fields gracefully', () => {
+      const table = buildTableFromAggs(
+        [{ testField: [{ key: 'key1', doc_count: 10 }] }],
+        [],
+      );
+      expect(table).toEqual({});
+    });
+
+    it('should handle empty data gracefully', () => {
       const fields = [{ field: 'testField', title: 'Test Field' }];
-      const table = buildTableFromAggs(data, fields);
+      const table = buildTableFromAggs([], fields);
       expect(table).toEqual({
-        'Test Field values': ['key1', 'key2'],
-        'Test Field total': [10, 20],
+        'Test Field values': [],
+        'Test Field total': [],
       });
     });
 
-    it('should handle second-level aggregations', () => {
+    it('should ignore fields with invalid properties', () => {
       const data = [
         {
-          testField: [
-            {
-              key: 'key1',
-              doc_count: 10,
-              secondLevelTestField: {
-                buckets: [
-                  { key: 'subKey1', doc_count: 5 },
-                  { key: 'subKey2', doc_count: 5 },
-                ],
-              },
-            },
-          ],
+          testField: [{ key: 'key1', doc_count: 10 }],
+          otherField: [{ key: 'key2', doc_count: 20 }],
         },
       ];
-      const fields = [
-        { field: 'testField', secondLevelAgg: 'secondLevelTestField' },
-      ];
+      const fields = [{}, { field: 'testField' }];
       const table = buildTableFromAggs(data, fields);
+      expect(table).not.toHaveProperty('otherField values');
       expect(table).toEqual({
         testField_values: ['key1'],
         testField_total: [10],
-        subKey1: [5],
-        subKey2: [5],
       });
     });
-
-    it('should return an empty object when non-array data is provided', () => {
-      const table = buildTableFromAggs({}, []);
-      expect(table).toEqual({});
+  });
+  it('should build a table from provided data and fields', () => {
+    const data = [
+      {
+        testField: [
+          { key: 'key1', doc_count: 10 },
+          { key: 'key2', doc_count: 20 },
+        ],
+      },
+    ];
+    const fields = [{ field: 'testField', title: 'Test Field' }];
+    const table = buildTableFromAggs(data, fields);
+    expect(table).toEqual({
+      'Test Field values': ['key1', 'key2'],
+      'Test Field total': [10, 20],
     });
+  });
+
+  it('should handle second-level aggregations', () => {
+    const data = [
+      {
+        testField: [
+          {
+            key: 'key1',
+            doc_count: 10,
+            secondLevelTestField: {
+              buckets: [
+                { key: 'subKey1', doc_count: 5 },
+                { key: 'subKey2', doc_count: 5 },
+              ],
+            },
+          },
+        ],
+      },
+    ];
+    const fields = [
+      { field: 'testField', secondLevelAgg: 'secondLevelTestField' },
+    ];
+    const table = buildTableFromAggs(data, fields);
+    expect(table).toEqual({
+      testField_values: ['key1'],
+      testField_total: [10],
+      subKey1: [5],
+      subKey2: [5],
+    });
+  });
+
+  it('should return an empty object when non-array data is provided', () => {
+    const table = buildTableFromAggs({}, []);
+    expect(table).toEqual({});
   });
 });
